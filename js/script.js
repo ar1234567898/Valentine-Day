184763e47b6c04b1572bf1d9bf37cb1d6542f47b
ar1234567898 <24a_tav@liceum.ztu.edu.ua>
2025-02-17 09:53:05 +0200

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+
+
+
+
+
+
+let wish = 
+
+
+
+
+document.getElementById('btnLoveWishes').addEventListener('click', function() {
+    // console.log(document.getElementsByClassName('btnLoveWishes'))
+    document.getElementById('loveWishes').innerText(wish)
+})
+
+
+
+
+
+
+wishes = [
+  'May love always shine brightly in your heart.',
+  'Wishing you warmth, care, and endless happiness with your special someone.',
+  'May your day be filled with romance and joy.',
+  'Love, harmony, and wonderful surprises to you on this special day.',
+  'May your heart sing with happiness and your soul be filled with love.',
+  'Wishing you sweet hugs, true feelings, and unforgettable moments.',
+  'May your life always have space for big and beautiful love.',
+  'Wishing you love that lifts you up and fills your life with meaning.',
+  'May romance and passion never leave your life.',
+  'Love, comfort, and the sweetest confessions today and always.',
+];
